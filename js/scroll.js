3594870495ca97c44ba6f643c9f094220427b835
DataJournalism-CUC <datajournalism_CUC@126.com>
2018-05-31 10:27:37 +0800

    add video

--- js/scroll.js
+++ js/scroll.js
@@ -11,14 +11,17 @@ $(document).ready(function(){
         .setPin("#content", {pushFollowers: false})
         .addIndicators({name:"headerfade"}).addTo(controller);
     
+    var trigV = new ScrollMagic.Scene({triggerElement:"#trigV", duration:"100%", triggerHook:0})
+            .on("enter", function(){$("#pin video").trigger("play")}).addIndicators({name:"video"}).addTo(controller);
     
     var trig0 = new ScrollMagic.Scene({
-        triggerElement:"#trigPre", duration:"300%",triggerHook:0 })
+        triggerElement:"#trigPre", duration:"600%",triggerHook:0 })
         .on("enter", preL()).setPin("#chart #chart2", {pushFollowers: false})
+        .setClassToggle("#sinfo1", "fade-in")
         .addIndicators({name: 'trig0',}).addTo(controller);
 
     var trig1 = new ScrollMagic.Scene({
-        triggerElement: "#trigFirst", duration: "100%", triggerHook: 0})
+        triggerElement: "#trigFirst", duration: "100%", triggerHook: 0.6})
         .on("enter", function(){
             d3.selectAll(".axis").transition()
                 .duration(0)
@@ -69,8 +72,22 @@ $(document).ready(function(){
         }).addIndicators({ name: "second" }).addTo(controller);
 
 
+    var trig2_3 = new ScrollMagic.Scene({
+        triggerElement:"#trig2-3", duration:"100%", triggerHook: 0.9
+    }).on("enter", function(){
+        d3.selectAll(".axis").transition()
+            .duration(0)
+            .remove();
+        d3.selectAll("circle")
+            .transition()
+            .duration(3000)
+            .style("opacity", 0)
+            .attr("cx", d => 200 + Math.random() * 1000)
+            .attr("cy", d => Math.random() * 1000);
+    }).addIndicators({name:"2-3"}).addTo(controller);
     var trig3 = new ScrollMagic.Scene({
-        triggerElement: "#trigThird", duration: "100%", triggerHook: 0})
+        triggerElement: "#trigThird", duration: "100%", triggerHook: 0.6})
+        .setClassToggle("#sinfo2", "fade-in")
         .on("enter", function(){
             d3.selectAll(".axis").transition()
                 .duration(0)
@@ -82,6 +99,25 @@ $(document).ready(function(){
                 .attr("cx", d => d.x3 + svgWidth - Twidth + margin.left)
                 .attr("cy", d => d.y3 + margin2.top * 2);
         }).addIndicators({name:"third"}).addTo(controller);
+
+    var trig4 = new ScrollMagic.Scene({
+        triggerElement: "#trigForth", duration: "100%", triggerHook: 0
+    })
+        .on("enter", function () {
+            d3.selectAll(".axis").transition()
+                .duration(0)
+                .remove();
+            d3.selectAll("circle")
+                .transition()
+                .duration(3000)
+                .style("opacity", 1)
+                .attr("cx", function(d){
+                    if("x4" in d){return d.x4}else{return 800 + Math.random()*100} 
+                })
+                .attr("cy", function (d) {
+                    if ("y4" in d) { return d.y4 } else { return 800 + Math.random() * 100 }
+                });
+        }).addIndicators({ name: "forth" }).addTo(controller);
     
 
     
